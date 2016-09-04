368c6b4b4115d1945250e765d27e06f0f4805c79
Cindy Panameno <cspanameno2013@gmail.com>
2016-09-04 15:24:04 -0700

    Finished hw

---
+++ recursion.py
@@ -0,0 +1,110 @@
+# --------- #
+# Recursion #
+# --------- #
+
+# 1. Write a function that uses recursion to print each item in a list.
+def print_item(my_list, i=0):
+    """Prints each item in a list recursively.
+
+        >>> print_item([1, 2, 3])
+        1
+        2
+        3
+
+    """
+    # if len(my_list) == 0:
+    #     return 
+
+    # print my_list[0]
+
+    if i < len(my_list):
+        print my_list[i]
+        print_item(my_list, i+1)
+
+
+# 2. Write a function that uses recursion to print each node in a tree.
+
+def print_all_tree_data(tree):
+    """Prints all of the nodes in a tree.
+
+
+        >>> class Node(object):
+        ...     def __init__(self, data):
+        ...             self.data=data
+        ...             self.children = []
+        ...     def add_child(self, obj):
+        ...             self.children.append(obj)
+        ...
+        >>> one = Node(1)
+        >>> two = Node(2)
+        >>> three = Node(3)
+        >>> one.add_child(two)
+        >>> one.add_child(three)
+        >>> print_all_tree_data(one)
+        1
+        2
+        3
+
+    """
+    print tree.data
+
+    for node in tree.children:
+        print_all_tree_data(node)
+
+# 3. Write a function that uses recursion to find the length of a list.
+
+
+def list_length(my_list):
+    """Returns the length of list recursively.
+        >>> list_length([1, 2, 3, 4])
+        4
+
+    """
+    #base case 
+    if len(my_list) == 0: 
+        return 0
+
+    return 1 + list_length(my_list[1:])
+
+
+# 4. Write a function that uses recursion to count how many nodes are in a tree.
+
+def num_nodes(tree):
+    """Counts the number of nodes.
+
+        >>> class Node(object):
+        ...     def __init__(self, data):
+        ...             self.data=data
+        ...             self.children = []
+        ...     def add_child(self, obj):
+        ...             self.children.append(obj)
+        ...
+        >>> one = Node(1)
+        >>> two = Node(2)
+        >>> three = Node(3)
+        >>> one.add_child(two)
+        >>> one.add_child(three)
+        >>> num_nodes(one)
+        3
+    """
+
+    if not tree: 
+        return 0
+    #start with at least one node if we hit this line
+    total = 1
+    
+    for node in tree.children:
+        total += num_nodes(node)
+    return total 
+
+#####################################################################
+# END OF ASSIGNMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
